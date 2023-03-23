0e6199585b626e2bbddaa2ebb029bbcd20bab425
Siderius <therealjonnye145@gmail.com>
2023-03-23 09:46:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,19 @@ symbols_value = {
 }
 
 def check_winnings(columns, lines, bet, values):
-
+    winnings = 0
+    winning_lines = []
+    for line in range(lines):
+        symbol = columns[0][line]
+        for column in columns:
+            symbol_to_check = column[line]  
+            if symbol != symbol_to_check:
+                break
+        else:
+            winnings += values[symbol] * bet
+            winning_lines.append(line + 1)
+    
+    return winnings, winning_lines
 
 def get_slot_machine_spin(rows, cols, symbols):
     all_symbols = []
@@ -96,8 +108,7 @@ def get_bet():
 
     return amount
 
-def main():
-    balance = deposit()
+def spin(balance):
     lines = get_the_number_of_line()
     while True:
         bet = get_bet()
@@ -111,5 +122,20 @@ def main():
 
     slots = get_slot_machine_spin(ROWS, COLS, symbols_count)
     print_slot_machines(slots)
+    winnings, winnings_lines = check_winnings(slots, lines, bet, symbols_value)
+    print(f"You won ${winnings}.")
+    print(f"You won on lines:", *winnings_lines)
+    return winnings - total_bet
 
+def main():
+    balance = deposit()
+    while True:
+        print(f"Current balance is ${balance}.")
+        answer = input("Press enter to play (q to quite).")
+        if answer == "q":
+            break
+        balance += spin(balance)
+    
+    print(f"You left with ${balance}.")
+    
 main()
